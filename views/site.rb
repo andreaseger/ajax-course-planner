fdc5300674e705a241a975321dbe768345bdd989
Eger Andreas <github@eger-andreas.de>
2012-05-10 19:58:54 +0200

    fixing view

--- views/site.rb
+++ views/site.rb
@@ -12,7 +12,7 @@ class CoursePlanner
         ENV['PLANNER_GA_ID']
       end
       def show_ga
-        ENV[:RACK_ENV] == 'production'
+        ENV['RACK_ENV'] == 'production'
       end
       def stylesheets_tag
         %{<link href="#{asset_path 'application.css'}" media="screen, projection" rel="stylesheet" type="text/css" />}
