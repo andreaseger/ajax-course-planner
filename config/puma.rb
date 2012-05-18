d08cc922a9a0d516381c2c8d070b7bf2788cc852
Eger Andreas <github@eger-andreas.de>
2012-05-19 01:43:15 +0200

    WIP cahgne puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
 APP_PATH = "/var/www/courses/"
 
-state_path APP_PATH + "/tmp/puma.state"
-pidfile APP_PATH + "/tmp/pids/puma.pid"
+state_path APP_PATH + "tmp/puma.state"
+pidfile APP_PATH + "tmp/pids/puma.pid"
 
-activate_control_app
+activate_control_app 'unix:///tmp/courses_pumactl.sock'
 bind "unix:///tmp/courses_puma.sock"
 
 threads 2, 10
